85904bcd7a17ad8384d9f967efa8082a83363dd3
Shuntian Liu <shuntian.liu@outlook.com>
2026-02-08 11:48:42 +0000

    minor update on mem.rs

--- benches/mem.rs
+++ benches/mem.rs
@@ -9,10 +9,12 @@ use crdt_bench::crdt::{Crdt, CrdtLib};
 #[global_allocator]
 static ALLOC: dhat::Alloc = dhat::Alloc;
 
+static FILE_SZ: &str = "6k";
+
 fn bench_mem(lib: CrdtLib) {
     let file_to_load = match lib {
-        CrdtLib::Automerge => "./benches/data/rnd-6k.am",
-        CrdtLib::Yrs => "./benches/data/rnd-6k.yrs",
+        CrdtLib::Automerge => format!("./benches/data/rnd-{}.am", FILE_SZ),
+        CrdtLib::Yrs => format!("./benches/data/rnd-{}.yrs", FILE_SZ),
     };
 
     let data = fs::read(file_to_load).unwrap();
@@ -22,13 +24,19 @@ fn bench_mem(lib: CrdtLib) {
     };
 
     let doc = doc.load(&data);
+    let stats = dhat::HeapStats::get();
+    println!("current allocation {:?}", stats);
+
     black_box(doc);
 }
 
 fn main() {
     let _profiler = dhat::Profiler::new_heap();
-    let lib = CrdtLib::Automerge;
+    let lib = CrdtLib::Yrs;
 
-    println!("memory profiling for {:?}", lib);
+    println!(
+        "memory profiling for {:?} by loading file size {}",
+        lib, FILE_SZ
+    );
     bench_mem(lib);
 }
